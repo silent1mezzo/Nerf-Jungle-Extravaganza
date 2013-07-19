87cd177cdf3e26c8e927808026fcf78860346153
Adam McKerlie <adammckerlie@gmail.com>
2013-07-19 08:16:55 -0400

    Easily drop table. Add Order By

--- js/storage.js
+++ js/storage.js
@@ -2,14 +2,19 @@ var db = window.openDatabase("scores", "1.0", "Stores the high scores for player
 
 function createTable() {
     db.transaction(function(tx) {
-        tx.executeSql("CREATE TABLE scores (id REAL UNIQUE, name TEXT, score TEXT)");
+        tx.executeSql("CREATE TABLE IF NOT EXISTS scores (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT, score TEXT)");
+    });
+}
+
+function dropTable() {
+    db.transaction(function(tx) {
+        tx.executeSql("DROP TABLE scores");
     });
 }
 
 function addScore(name, score) {
-    var num = Math.round(Math.random() * 100000);
     db.transaction(function(tx) {
-        tx.executeSql("INSERT INTO scores (id, name, score) VALUES (?, ?, ?)", [num, name, score]);
+        tx.executeSql("INSERT INTO scores (name, score) VALUES (?, ?)", [name, score]);
     });
 }
 
@@ -21,7 +26,7 @@ function deleteScore(id) {
 
 function showScores() {
     db.transaction(function(tx) {
-        tx.executeSql("SELECT * FROM scores ORDER BY score", [], function(tx, result) {
+        tx.executeSql("SELECT * FROM scores order by score DESC", [], function(tx, result) {
             for (var i = 0, item = null; i < result.rows.length; i++) {
                 item = result.rows.item(i);
                 console.log(item['name'] + ": " + item['score']);
@@ -31,7 +36,7 @@ function showScores() {
 }
 
 db.transaction(function(tx) {
-    tx.executeSql("SELECT * FROM scores ORDER BY score", [], function(tx, result) {
+    tx.executeSql("SELECT * FROM scores ORDER BY score DESC", [], function(tx, result) {
         for (var i = 0, item = null; i < result.rows.length; i++) {
             item = result.rows.item(i);
             document.getElementById('scores').innerHTML +=
